d02aacbafd8789c9a69145549be3bf33cc1a5885
Jdoe1234 <tino190991@live.dk>
2018-05-31 23:43:49 +0200

    js slideshow + spacer removal
    frontpage has been updated with an interactive tile slideshow
    
    all spacers have been removed and replaced by margin styling classes

---
+++ assets/js/index-slide.js
@@ -0,0 +1,28 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+
+
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-red", "");
+  }
+  dots[slideIndex-1].className += " w3-red";
+}
